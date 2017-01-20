fff4329ae1cdf964a8748d91ebfef5d681e16ce0
rmzse <rodrigo@therawdelicious.com>
2017-01-20 08:29:58 +0100

    add performance data controller, test failing: activemodel forbidden attribute err

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 		namespace :v1, defaults: { format: :json } do
 			mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 			resources :performance_data, only: [:create]
-		end 
+		end
 	end
 end
